ed4fba55668237f02c872af9f0ca6a78c32f86c5
Eric Fiorani <efiorani305@gmail.com>
2024-02-24 11:18:36 -0500

    created billing, business, button and cardDeal components

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>Button</div>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient rounded-[10px] text-[18px] text-primary outline-none font-medium font-poppins ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
